c0b8a4178c652d9fd119b4d89aecba7f1a7afdd4
Ayana Yedilbay <ayanayedilbay@gmail.com>
2022-10-22 09:32:29 -0500

    changes from runner

--- src/test/java/com/test/automationexercise/runners/Runner.java
+++ src/test/java/com/test/automationexercise/runners/Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features/automationexercise",
         glue = "com/test/automationexercise/stepdefinition",
-        dryRun = true,
+        dryRun = false,
         tags = "",
         plugin = {"pretty","html:target/uiReport.html","rerun:target/uiFailedTests.txt"}
 
